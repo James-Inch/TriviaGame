4b5bb2d50606587035114f37233664c7df80f96c
james inch <james.inch0103@gmail.com>
2018-07-31 17:24:53 -0400

    add object for Q's and A's

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,48 @@
+console.log("test")
+$(document).ready(function () {
+  $("startBtn").on("click", function(){
+    
+  });   
+
+  // State manangement variables
+  var correctAnswers = 0;
+  var incorrectAnswers = 0;
+
+  
+  
+  // set initial timer
+
+});
+// create an array of objects for the questions, answer choices and correct answer
+var questions = [{
+      question: "What is the name of the gaget that Rick uses to travel between dimensions and universes?",
+      answers: ["","","",""],
+      correctAnswers: "Portal Gun"},
+    {
+      question: "What is a schmeckle?",
+      answers: ["","","",""],
+      correctAnswers: "A currency"},
+    {
+      question:"What is the name of the rock band Rick and his best friend were in back in the 20's?",
+      answers:["","","",""],
+      correctAnswers: "'The Flesh Curtains'"},
+    {
+      question: "What are the 'ball fondlers'?",
+      answers: ["","","",""],
+      correctAnswers: "Rick and Morty’s favorite TV show"},
+    {
+      question: "what does the microscope that the devil gave Rick do?",
+      answers: ["","","",""],
+      correctAnswers: "Makes people retarded"
+}];
+
+console.log(questions.question[0]);
+
+
+
+
+
+
 // * You'll create a trivia game that shows only one question until the player answers it or their time runs out.
 
 // * If the player selects the correct answer, show a screen congratulating them for choosing the right option. After a few seconds, display the next question -- do this without user input.
@@ -8,19 +53,3 @@
 //   * If the player chooses the wrong answer, tell the player they selected the wrong option and then display the correct answer. Wait a few seconds, then show the next question.
 
 // * On the final screen, show the number of correct answers, incorrect answers, and an option to restart the game (without reloading the page).
-$(document).ready(function () {
-
-// State manangement variables
-var correctAnswers = 0;
-var incorrectAnswers = 0;
-
-
-// set initial timer
-function fiveSeconds() {
-
-    // in the element with an id of time-left add an h2 saying About 10 Seconds Left!
-    // console log 10 seconds left
-    $("#time-left").append("<h2>About 10 Seconds Left!</h2>");
-    console.log("10 seconds left");
-  }
-});
